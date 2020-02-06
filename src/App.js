5c9877c6eb885f38d9222182ad30ba7adbfbfb98
Patrycja <Patrycjaqtno05@gmail.com>
2020-02-06 23:08:10 +0100

    create graphics icon gender

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'; 
 
+function IconSexDisplay(bool) {
+  if(bool === "true")  {
+    return <img src="./female_icon.png"></img>
+  }
+  else return <img src = "./male_icon.png"></img>
+}
 
 class App extends Component {
   constructor(props){
@@ -18,19 +24,19 @@ class App extends Component {
           {clientsList : res.data})
       })
   }
- 
+  
   render() {
 
     return this.state.clientsList.map(clientsList => {
       return (
         <div className='listOfPeopleContainer'>
-          <div className = "containerPeople" key={clientsList.id}> 
+          <div className = "containerPeople" key={clientsList._id}> 
             <div className = "image-container">
               <img src={clientsList.avatar}></img>
             </div>
 
             <div className = "info-container">
-              <h2>{clientsList.name}, <span className = "ageParagraph">{clientsList.age}</span> lat <span className = "sex">{clientsList.sex}</span></h2>
+              <h2>{clientsList.name}, <span className = "ageParagraph">{clientsList.age}</span> lat <span className = "sexIcon">{IconSexDisplay(JSON.stringify(clientsList.sex))}</span></h2>
               <p>{clientsList.address.city}, {clientsList.address.street} {clientsList.address.houseNumber}</p>
               <p className = 'emailParagraph'>{clientsList.email} </p>
             </div>
